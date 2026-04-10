cb4a20d5e58338b0fb0ddbce595c1b8f0ea153a3
thanhvinh522 <duongthanhvinh522@gmail.com>
2026-04-11 01:19:43 +0700

    update collection in java

---
+++ array-demo/array_with_toString/src/Main.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        //definition a array
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
